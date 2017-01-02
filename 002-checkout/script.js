8325c0c4a4484667e294223bca15048acdbab706
Andrea Simes <avs2151@columbia.edu>
2017-01-01 20:19:26 -0800

    Added algo to correctly space credit card numbers

--- 002-checkout/script.js
+++ 002-checkout/script.js
@@ -9,35 +9,42 @@
 
 var card_type; //global to hold credit card type
 
+/*these values come from Wikipedia https://en.wikipedia.org/wiki/Payment_card_number*/
+
 var cards = {"amex":[[34],[37]], "discover":[[6011],[622126, 622925],[644, 649], [65]], "mastercard":[[50, 55]], "visa":[[4]] }
 var cardnames = Object.keys(cards);
 
 $('input#cardnumber').on('input', function() { 
     var cardnames = Object.keys(cards);
     var input = $('input#cardnumber').val();
-    console.log("input " + typeof input);
-    if (input.includes("10")){
-        console.log("hi");
-    }
+    //console.log("input " + typeof input);
+   
     var result;
     result = cardQuerier(input);
-    console.log("result: " + result);
+    //console.log("result: " + result);
     $("input#cardnumber").css('background-color', '#FFFFFF');
     if (result == undefined){
          $("input#cardnumber").css('background', 'url("./Visa.png") no-repeat scroll 490px 4px/44.8px 28px, url("./AMEX.png") no-repeat scroll 590px 4px/44.8px 28px, url("./MC.png") no-repeat scroll 540px 4px/44.8px 28px, url("./Discover.png") no-repeat scroll 640px 4px/44.8px 28px');
     }
     if (result == "mastercard"){
-        $("input#cardnumber").css('background', 'url("./MC.png") no-repeat scroll 640px 4px/44.8px 28px'); //reset field with corresponding card icon
+        $("input#cardnumber").css('background', 'url("./MC.png") no-repeat scroll 440px 4px/44.8px 28px'); //reset field with corresponding card icon
     }
     if (result == "visa"){
-         $("input#cardnumber").css('background', 'url("./Visa.png") no-repeat scroll 640px 4px/44.8px 28px'); //reset field with corresponding card icon
+         $("input#cardnumber").css('background', 'url("./Visa.png") no-repeat scroll 440px 4px/44.8px 28px'); //reset field with corresponding card icon
     }
     if (result == "discover"){
-         $("input#cardnumber").css('background', 'url("./Discover.png") no-repeat scroll 640px 4px/44.8px 28px'); //reset field with corresponding card icon
+         $("input#cardnumber").css('background', 'url("./Discover.png") no-repeat scroll 440px 4px/44.8px 28px'); //reset field with corresponding card icon
     }
     if (result == "amex"){
-         $("input#cardnumber").css('background', 'url("./AMEX.png") no-repeat scroll 640px 4px/44.8px 28px'); //reset field with corresponding card icon
+         $("input#cardnumber").css('background', 'url("./AMEX.png") no-repeat scroll 440px 4px/44.8px 28px'); //reset field with corresponding card icon
     }
+    
+    
+    input = numberSpacer(input);
+    console.log("input" + input);
+    
+    $("input#cardnumber").val(input);
+    
     $("input#cardnumber").css('background-color', '#FFFFFF'); //reset default format
     $("input#cardnumber").css('border', '1px solid');
     $("input#cardnumber").css('border-color', '#B0B0B0');
@@ -49,12 +56,13 @@ $('input#name').on('input', function() {
     console.log("typed something");
     console.log($('input#name').val());
     if($('input#name').val() == ""){ //no input
-        $("input#name").css('background', 'url("./x.png") no-repeat scroll 660px 8px/19.2px 19.25px'); //display "x" icon
+        $("input#name").css('background', 'url("./x.png") no-repeat scroll 460px 8px/19.2px 19.25px'); //display "x" icon
         $('input#name').tooltip(); //instantiate tooltip to help user deal with error
     }
     else {
         $("input#name").css('background', '');
-        $('input#name').tooltip('dispose')
+        $('input#name').tooltip('dispose');
+        $("input#name").css('border-color', '#B0B0B0');
     }
     $("input#name").css('background-color', '#FFFFFF');
 });
@@ -65,8 +73,8 @@ $('input#name').on('input', function() {
 $('input#expiration').on('input', function() {
     console.log("typed something");
     console.log($('input#name').val());
-    if($('input#expiration').val() == "" || $('input#expiration').val().length > 4 || $('input#expiration').val().length < 4){ //invalid inputs
-        $('input#expiration').css('background', 'url("./x.png") no-repeat scroll 165px 8px/19.2px 19.25px'); //"x" icon
+    if($('input#expiration').val() == "" || $('input#expiration').val().length > 4 || $('input#expiration').val().length < 4 || $('input#expiration').val().match(/([^\d])+/) ){ //invalid inputs
+        $('input#expiration').css('background', 'url("./x.png") no-repeat scroll 64px 8px/19.2px 19.25px'); //"x" icon
         $('input#expiration').tooltip(); //instantiate tooltip to help user deal with error
     }
     else {
@@ -79,10 +87,10 @@ $('input#expiration').on('input', function() {
 /* SECURITY CODE */
 
 $('input#security').on('input', function() {
-    console.log("typed something");
-    console.log($('input#name').val());
-    if($('input#security').val() == ""){ //no input
-        $('input#security').css('background', 'url("./x.png") no-repeat scroll 165px 8px/19.2px 19.25px'); //"x" icon
+    //console.log("typed something");
+    //console.log($('input#name').val());
+    if($('input#security').val() == "" || $('input#security').val().match(/([^\d])+/) || $('input#security').val().length > 3 || $('input#security').val().length < 3 ){ //no input
+        $('input#security').css('background', 'url("./x.png") no-repeat scroll 45px 8px/19.2px 19.25px'); //"x" icon
         $('input#security').tooltip(); //instantiate tooltip to help user deal with error
     }
     else {
@@ -92,42 +100,72 @@ $('input#security').on('input', function() {
     $('input#security').css('background-color', '#FFFFFF');
 });
 
-
-$(function () {
+//initialize popover
+$(function () { 
     $('[data-toggle="popover"]').popover();
 });
 
+//method to add spaces between each group of 4 digits
 
-function cardQuerier(input) {
+function numberSpacer(num){ 
+    num = removeWhitespace(num);
+    console.log("initial numberspacer " + num);
+    
+    if (num.length > 4){
+        for (var y = 0; y < num.length; y += 1){
+            console.log(y + " " + num);
+            console.log(y + "charAt(y)" + num.charAt(y));
+            if (y%5 == 3){
+                num = num.substr(0, y+1) + " " + num.substr(y+1, num.length);
+            } 
+        }
+    }
+    return num;
+}
+
+function removeWhitespace(n){ //recursive helper method to remove whitespace
+    if(n.match(/[\s]/)){
+        var whiteindex = /\b([\s])+/g.exec(n);
+        console.log("n" + n);
+        console.log("whiteindex " + whiteindex.index);
+        n = n.substr(0,whiteindex.index) + n.substr(whiteindex.index+1, n.length);
+        return removeWhitespace(n); //recursive call
+    }
+    else { //base case
+        return n;
+    }
+    
+}
+
+function cardQuerier(input) { //assign correct cardname based on number
     var cardname;
     for (var i = 0; i < cardnames.length; i += 1){
         cardname = cardnames[i];
         //console.log(cards[cardnames[i]]);
         for (var z = 0; z < cards[cardnames[i]].length; z += 1){
-             
             //console.log(cards[cardnames[i]][z]);
             var current = cards[cardnames[i]][z];
             if (current.length > 1){ //multiple elements
-                console.log("more than 1 " + current);
-                console.log(range(cards[cardnames[i]][z][0], cards[cardnames[i]][z][1]));
+                //console.log("more than 1 " + current);
+                //console.log(range(cards[cardnames[i]][z][0], cards[cardnames[i]][z][1]));
                 var gen_array = range(cards[cardnames[i]][z][0], cards[cardnames[i]][z][1]);
                 for (var t = 0; t < gen_array.length; t += 1){
                     var len = String(gen_array[t]).length;
                     if(input.substr(0,len).includes(String(gen_array[t]))){
-                        console.log("yay!");
+                      
                         cardname = cardnames[i];
-                        console.log(cardname);
+                        //console.log(cardname);
                         return cardname;
                     }
                 }
 
             }
             else{ //only one element in array
-                console.log("current" + typeof current[0]);
+                //console.log("current" + typeof current[0]);
                 if(input.includes(String(current[0]))){
-                        console.log("yay!");
+                        //console.log("yay!");
                         cardname = cardnames[i];
-                        console.log(cardname);
+                        //console.log(cardname);
                         return cardname;
                     }
             }
